54780ba627a0b2a860a6a59d1805697540272e3f
Megan Ma <menghanma@gmail.com>
2020-09-27 18:46:36 -0700

    Multiply a matrix by an integer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 final static Scanner scanner = new Scanner(System.in);
 
+    // add two matrices with integer elements
     private static void add (int[][] matrix1, int[][] matrix2, int rows, int columns){
         int[][] addition = new int[rows][columns];
         for (int i = 0; i < matrix1.length; i++) {
@@ -14,6 +15,17 @@ final static Scanner scanner = new Scanner(System.in);
             }
         }
     }
+
+    // multiply a matrix by a constant
+    private static void multiply (int[][] matrix, int factor) {
+        for (int[] ints : matrix) {
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(j == ints.length - 1 ?
+                        ints[j] * factor + "\n" : ints[j] * factor + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // first matrix
         int row1 = scanner.nextInt();
@@ -24,20 +36,22 @@ final static Scanner scanner = new Scanner(System.in);
                 matrix1[i][j] = scanner.nextInt();
             }
         }
-        // second matrix
-        int row2 = scanner.nextInt();
-        int column2 = scanner.nextInt();
-        int[][] matrix2 = new int[row2][column2];
-        for (int i = 0; i < row2; i++) {
-            for (int j = 0; j < column2; j++) {
-                matrix2[i][j] = scanner.nextInt();
-            }
-        }
-
-        if (row1 != row2 || column1 != column2) {
-            System.out.println("ERROR");
-        } else {
-            add(matrix1, matrix2, row1, column1);
-        }
+        int factor = scanner.nextInt();
+        multiply(matrix1, factor);
+//        // second matrix
+//        int row2 = scanner.nextInt();
+//        int column2 = scanner.nextInt();
+//        int[][] matrix2 = new int[row2][column2];
+//        for (int i = 0; i < row2; i++) {
+//            for (int j = 0; j < column2; j++) {
+//                matrix2[i][j] = scanner.nextInt();
+//            }
+//        }
+//
+//        if (row1 != row2 || column1 != column2) {
+//            System.out.println("ERROR");
+//        } else {
+//            add(matrix1, matrix2, row1, column1);
+//        }
     }
 }
